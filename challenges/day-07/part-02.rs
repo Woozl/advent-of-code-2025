180aefd439f5cf45f3a0e9456a2d510b128c0868
David Glymph <Woozl@users.noreply.github.com>
2025-12-19 23:08:52 -0500

    day 7 pt 2, refactor and format

--- challenges/day-07/part-02.rs
+++ challenges/day-07/part-02.rs
@@ -24,23 +24,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect();
 
+    // work from the bottom of the grid up, consider all bottom cells as starting positions
     grid.last_mut().unwrap().fill(Cell::Value(1));
 
-    'row_loop: for row_idx in (1..grid.len()).rev() {
+    'o: for row_idx in (1..grid.len()).rev() {
+        // get a mutable reference to the current row (curr_row) to update the grid's values
+        // last_row is a immutable ref to the row directly below, used to generate values for the current row
         let (before, after) = grid.split_at_mut(row_idx);
         let last_row: &Vec<Cell> = &after[0];
         let curr_row: &mut Vec<Cell> = &mut before[before.len() - 1];
 
         for (col_idx, cell) in curr_row.iter_mut().enumerate() {
-            let next_cell: Cell = match cell {
+            // update the curr_row based on the last_row
+            *cell = match cell {
+                // if the cell is the start cell, we've found the solution.
+                // just use the value from the cell right below
+                //   . S .
+                //   1 2 1  -> solution: 2
+                //   1 1 1
                 Cell::Start => {
-                    if let Cell::Value(val) = &last_row[col_idx] {
-                        println!("{}", val);
-                    } else {
+                    let Cell::Value(val) = &last_row[col_idx] else {
                         panic!("Cell below start didn't have a value");
-                    }
-                    break 'row_loop;
+                    };
+                    println!("{}", val);
+                    break 'o;
                 }
+
+                // if the cell is a splitter, it changes into a Value that is
+                // the sum of the bottom left and bottom right cell values
+                //   . ^ .   becomes   . 3 .
+                //   1 1 2      ->     1 1 2
                 Cell::Splitter => {
                     let left = &last_row[col_idx - 1];
                     let right = &last_row[col_idx + 1];
@@ -50,9 +63,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         panic!("Cells on rows below splitter were not values");
                     }
                 }
+
+                // this is wildcard to satisfy Rust's type checker, but this
+                // branch should only ever handle `Empty`s.
+                //
+                // if a cell is empty, copy the values from the previous row up
+                //   . . .   becomes   1 2 1
+                //   1 2 1      ->     1 2 1
                 _ => last_row[col_idx].clone(),
             };
-            *cell = next_cell;
         }
     }
 
